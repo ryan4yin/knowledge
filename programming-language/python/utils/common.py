1d72e9849ae56c651d67406ea718ed4ad8b94532
ryan <xiaoyin_c@qq.com>
2020-06-20 10:24:41 +0800

    fix: zip 会在遇到 StopIteration 时立即结束

--- programming-language/python/utils/common.py
+++ programming-language/python/utils/common.py
@@ -12,21 +12,37 @@ logger = logging.getLogger(__name__)
 """一些通用的实用函数"""
 
 
-def group_each(a, size: int, allow_none=False):
+def iter_one_by_one(items, stop_immediately=False):
     """
-        将一个可迭代对象 a 内的元素, 每 size 个分为一组
+    每次顺序从每个 item 中取出一个元素，跳过已经被迭代完毕的 item.
+    :param stop_immediately:  如果捕获到 StopIteration（某个 item 的末尾），就立即结束迭代。
+    """
+    iters = list(map(iter, items))
+    while len(iters):
+        for i, it in enumerate(iters):
+            try:
+                yield next(it)
+            except StopIteration:
+                if stop_immediately:
+                    return  # 立即停止迭代
+                
+                iters.pop(i)  # 跳过已经迭代完毕的 item
+
+
+def group_each(a, size: int, padding_with_none=False):
+    """
+        将一个可迭代对象 a 内的元素, 每 size 个分为一组。
         group_each([1,2,3,4], 2) -> [(1,2), (3,4)]
+
+        :param padding_with_none: 如果最后一组不足 size 个，是否用 None 补足
     """
-    func = zip_longest if allow_none else zip
-
-    iterators = [iter(a)] * size  # 将新构造的 iterator 复制 size 次（浅复制）
-    return func(*iterators)  # 然后 zip
-
-
-def iter_one_by_one(items, allow_none=False):
-    func = zip_longest if allow_none else zip
-
-    return chain.from_iterable(func(*items))
+    one_by_one_list = list(iter_one_by_one(a))
+    for i in range(0, len(one_by_one_list), 3):
+        upbound = i + 3
+        if padding_with_none and upbound > len(one_by_one_list):
+            yield one_by_one_list[i:upbound] + [None,]*(upbound-len(one_by_one_list))
+        else:
+            yield one_by_one_list[i:upbound]
 
 
 def filter_truth(items):
