87528c4dc770f68dcbae58b13fba30f056a98d11
ryan4yin <xiaoyin_c@qq.com>
2022-10-08 12:30:33 +0800

    feat: update

--- pl/python/date_and_time.py
+++ pl/python/date_and_time.py
@@ -1,1 +1,1 @@
-已迁移至 <https://thiscute.world/posts/python-tips-and-tricks/#2-%E6%97%B6%E9%97%B4%E6%97%A5%E6%9C%9F%E5%A4%84%E7%90%86>
+# 已迁移至 <https://thiscute.world/posts/python-tips-and-tricks/#2-%E6%97%B6%E9%97%B4%E6%97%A5%E6%9C%9F%E5%A4%84%E7%90%86>
